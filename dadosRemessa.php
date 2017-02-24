1bf897bd0eec46259d01e55c815ca6d3d36a4142
andre <andre@192.168.0.29>
2017-02-24 17:01:26 -0300

    Gerando Remessa

---
+++ dadosRemessa.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of dadosRemessa
+ *
+ * @author andre
+ */
+class dadosRemessa {
+    private $numLinha;
+    private $fieldValue;
+    private $posIni;
+    private $posEnd;
+    private $dataToStr;
+    
+    
+    
+}
---
+++ fileGenerate.php
@@ -0,0 +1,45 @@
+<?php
+class fileGenerate{    
+    private $pathFile;
+    private $fileName;
+    private $fileData;        
+    
+    function getPathFile() {
+        return $this->pathFile;
+    }
+    function getFileName() {
+        return $this->fileName;
+    }
+    function getFileData() {
+        return $this->fileData;
+    }
+    function setPathFile($pathFile) {
+        $this->pathFile = $pathFile;
+    }
+    function setFileName($fileName) {
+        $this->fileName = $fileName;
+        if(empty($fileName)){
+            $this->fileName = date('Ymd')."txt";
+        }        
+    }
+    function setFileData($fileData) {
+        $this->fileData = $fileData;
+    }
+    function __construct() {
+        $this->fileName = date('Ymd')."txt";
+    }    
+    function teste(){
+        print $this->pathFile."/".$this->fileName;
+    }    
+    function fileGenerate(){
+        $x = $this->pathFile."/".$this->fileName;
+        $file = fopen($x, 'w');
+        fwrite($file, $this->fileData);
+        fclose($file);
+    }    
+}
+
+$f = new fileGenerate();
+$f->setFileData("00000000");
+$f->setPathFile("remessa");
+$f->fileGenerate();
