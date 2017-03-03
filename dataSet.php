f39ff9297a0511f69bcad12aa15aebdc8bfe06bb
andre <andre@192.168.0.29>
2017-03-03 17:55:52 -0300

    addField

--- dataSet.php
+++ dataSet.php
@@ -4,27 +4,50 @@
 
 $dataSet = 
         array(
-            "CodBancoComp_G001"=>array("nCmp"=>"1.0","posInicio"=>"1","posFim"=>"3","leng"=>"3","Dec"=>"","type"=>"Num","Default"=>"001")
-            ,"LoteServico_G002"=>array("nCmp"=>"2.0","posInicio"=>"4","posFim"=>"7","leng"=>"4","Dec"=>"","type"=>"Num","Default"=>"0000")
-            ,"TipoRegistro_G003"=>array("nCmp"=>"3.0","posInicio"=>"8","posFim"=>"8","leng"=>"1","Dec"=>"","type"=>"Num","Default"=>"0")
-            ,"FEBRABAM_G004"=>array("nCmp"=>"4.0","posInicio"=>"9","posFim"=>"17","leng"=>"9","Dec"=>"","type"=>"Alpha","Default"=>"")
-            ,"tpPessoa_G005"=>array("nCmp"=>"5.0","posInicio"=>"18","posFim"=>"18","leng"=>"1","Dec"=>"","type"=>"Numeric","Default"=>"2")
-            ,"tpPessoaNum_G006"=>array("nCmp"=>"6.0","posInicio"=>"19","posFim"=>"32","leng"=>"14","Dec"=>"","type"=>"Numeric","Default"=>"")
-            ,"ConvBanco_G007"=>array("nCmp"=>"7.1.0","posInicio"=>"33","posFim"=>"52","leng"=>"20","Dec"=>"","type"=>"Alpha","Default"=>"")
-            ,"ConvBanco_G008"=>array("nCmp"=>"7.2.0","posInicio"=>"33","posFim"=>"52","leng"=>"20","Dec"=>"","type"=>"Alpha","Default"=>"")
+            "CodBancoComp_G001"=>array("nCmp"=>"1.0","posInicio"=>"1","posFim"=>"3","leng"=>"3","Dec"=>"","type"=>"Num","Default"=>"001","value"=>"")
+            ,"LoteServico_G002"=>array("nCmp"=>"2.0","posInicio"=>"4","posFim"=>"7","leng"=>"4","Dec"=>"","type"=>"Num","Default"=>"0000","value"=>"")
+            ,"TipoRegistro_G003"=>array("nCmp"=>"3.0","posInicio"=>"8","posFim"=>"8","leng"=>"1","Dec"=>"","type"=>"Num","Default"=>"0","value"=>"")
+            ,"FEBRABAM_G004"=>array("nCmp"=>"4.0","posInicio"=>"9","posFim"=>"17","leng"=>"9","Dec"=>"","type"=>"Alpha","Default"=>"","value"=>"")
+            ,"tpPessoa_G005"=>array("nCmp"=>"5.0","posInicio"=>"18","posFim"=>"18","leng"=>"1","Dec"=>"","type"=>"Numeric","Default"=>"2","value"=>"")
+            ,"tpPessoaNum_G006"=>array("nCmp"=>"6.0","posInicio"=>"19","posFim"=>"32","leng"=>"14","Dec"=>"","type"=>"Numeric","Default"=>"","value"=>"")
+            ,"ConvBanco_G007"=>array("nCmp"=>"7.1.0","posInicio"=>"33","posFim"=>"52","leng"=>"20","Dec"=>"","type"=>"Alpha","Default"=>"","value"=>"")
+            ,"ConvBanco_G008"=>array("nCmp"=>"7.2.0","posInicio"=>"33","posFim"=>"52","leng"=>"20","Dec"=>"","type"=>"Alpha","Default"=>"","value"=>"")
         );  
 
-//$dataSourse=array(
-//    "CodBancoComp_G001"=>"0001"
-//    ,"LoteServico_G002"=>""
-//    ,"TipoRegistro_G003"=>""
-//    ,"FEBRABAM_G004"=>""
-//    ,"tpPessoa_G005"=>""
-//    ,"tPessoaNum_G006"=>""
-//    ,"ConvBanco_G007"=>""
-//    ,"ConvBanco_G008"=>""
-//);
+function wordMatch($words, $input, $sensitivity){ 
+    $shortest = -1; 
+    foreach ($words as $word) {             
+        //verifica a similaridade entre palavras
+        $lev = levenshtein($input, $word); 
+        if ($lev == 0) { 
+            $closest = $word; 
+            $shortest = 0; 
+            break; 
+        } 
+        if ($lev <= $shortest || $shortest < 0) { 
+            $closest  = $word; 
+            $shortest = $lev; 
+        } 
+    } 
+    if($shortest <= $sensitivity){ 
+        return $closest; 
+    } else { 
+        return 0; 
+    } 
+}
 
+function addField($field,$value,$dataSet){    
+    if(array_key_exists($field, $dataSet)){
+        $dataSet[$field]["value"]=$value;
+        return $dataSet[$field];
+    }else{        
+        $words  = array_keys($dataSet);
+        $msg = "Campo ".$field." inexistente, o mais proximo seria o campo ".wordMatch($words, $field, 2);
+        echo $msg;
+    }
+}
+
+addField("ConvBanco_G007", "JURIDICA", $dataSet);
 
 class dataSource{
     private $fieldName;
@@ -32,7 +55,9 @@ class dataSource{
     private $dataSet;
     private $State="dsInactive";
     private $Line=[];
-        
+    
+
+
     public function __construct($params = []){
         if(empty($params)){
             throw new Exception("Informe o dataSet");
@@ -53,7 +78,7 @@ class dataSource{
             }            
         }        
     }
-    public function addField(){
+    public function addField($fieldName){
         
     }
 
--- teste.php
+++ teste.php
@@ -1,7 +1,73 @@
 <?php
 
-$name = 'arquivo.txt';
-$text = var_export($_POST, true);
-$file = fopen($name, 'a');
-fwrite($file, $text);
-fclose($file);
+
+//class changeType{
+//    private $fieldName;
+//    private $value;   
+//    
+//    public function AsString($param){
+//        $this->value = str_pad($param, 10, "-=", STR_PAD_LEFT); 
+//        return $this->property1;
+//    }    
+//    
+//    
+//}
+//
+//class SubMain{
+//    private $property2;
+//    private $FieldName;
+//    private $keyValue;
+//    
+//    public function __construct() {
+//        $this->property2 = new changeType();
+//    }        
+//    public function fieldByName($fieldName){
+//        $this->FieldName = $fieldName;
+//        return $this->property2;
+//    }
+//    private function _add(){
+//        
+//    }
+//    
+//}
+//
+//$x = new SubMain();
+////var_dump($x->fieldByName("CampoX")->getProperty1());
+
+
+$dataSet = 
+        array(
+            "CodBancoComp_G001"=>array("nCmp"=>"1.0","posInicio"=>"1","posFim"=>"3","leng"=>"3","Dec"=>"","type"=>"Num","Default"=>"001","value"=>"")
+            ,"LoteServico_G002"=>array("nCmp"=>"2.0","posInicio"=>"4","posFim"=>"7","leng"=>"4","Dec"=>"","type"=>"Num","Default"=>"0000","value"=>"")
+            ,"TipoRegistro_G003"=>array("nCmp"=>"3.0","posInicio"=>"8","posFim"=>"8","leng"=>"1","Dec"=>"","type"=>"Num","Default"=>"0","value"=>"")
+            ,"FEBRABAM_G004"=>array("nCmp"=>"4.0","posInicio"=>"9","posFim"=>"17","leng"=>"9","Dec"=>"","type"=>"Alpha","Default"=>"","value"=>"")
+            ,"tpPessoa_G005"=>array("nCmp"=>"5.0","posInicio"=>"18","posFim"=>"18","leng"=>"1","Dec"=>"","type"=>"Numeric","Default"=>"2","value"=>"")
+            ,"tpPessoaNum_G006"=>array("nCmp"=>"6.0","posInicio"=>"19","posFim"=>"32","leng"=>"14","Dec"=>"","type"=>"Numeric","Default"=>"","value"=>"")
+            ,"ConvBanco_G007"=>array("nCmp"=>"7.1.0","posInicio"=>"33","posFim"=>"52","leng"=>"20","Dec"=>"","type"=>"Alpha","Default"=>"","value"=>"")
+            ,"ConvBanco_G008"=>array("nCmp"=>"7.2.0","posInicio"=>"33","posFim"=>"52","leng"=>"20","Dec"=>"","type"=>"Alpha","Default"=>"","value"=>"")
+        );
+
+function wordMatch($words, $input, $sensitivity){ 
+    $shortest = -1; 
+    foreach ($words as $word) {             
+        //verifica a similaridade entre palavras
+        $lev = levenshtein($input, $word); 
+        if ($lev == 0) { 
+            $closest = $word; 
+            $shortest = 0; 
+            break; 
+        } 
+        if ($lev <= $shortest || $shortest < 0) { 
+            $closest  = $word; 
+            $shortest = $lev; 
+        } 
+    } 
+    if($shortest <= $sensitivity){ 
+        return $closest; 
+    } else { 
+        return 0; 
+    } 
+}
+$word = 'CodncoComp_G001'; 
+$words  = array_keys($dataSet);
+echo wordMatch($words, $word, 2);
