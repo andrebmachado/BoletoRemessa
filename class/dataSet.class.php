104cc5d69c683a42ae3acee7221c1c45f5aab8c0
andre <andre@192.168.0.29>
2017-03-08 17:58:23 -0300

    DataTypes

--- class/dataSet.class.php
+++ class/dataSet.class.php
@@ -50,7 +50,14 @@ $headerArquivo =
             ,"Mensagem2_C073"=>array("nCmp"=>"19.1","posInicio"=>"144","posFim"=>"183","leng"=>"40","Dec"=>"","type"=>"Alpha","Default"=>" ","valueReplace"=>" ") 
             ,"NumRemRet_G079"=>array("nCmp"=>"20.1","posInicio"=>"184","posFim"=>"191","leng"=>"8","Dec"=>"","type"=>"Num","Default"=>" ","valueReplace"=>" ") 
             ,"DataGravRemRet_G068"=>array("nCmp"=>"21.0","posInicio"=>"192","posFim"=>"199","leng"=>"8","Dec"=>"","type"=>"Num","Default"=>"0","valueReplace"=>"")/*DDMMAAAA nao obrigatorio informar zeros*/
-            ,"DataCredito_C003"=>array("nCmp"=>"21.0","posInicio"=>"192","posFim"=>"199","leng"=>"8","Dec"=>"","type"=>"Num","Default"=>"0","valueReplace"=>"")/*DDMMAAAA nao obrigatorio informar zeros*/
+            ,"DataCredito_C003"=>array("nCmp"=>"21.0","posInicio"=>"200","posFim"=>"207","leng"=>"8","Dec"=>"","type"=>"Num","Default"=>"        ","valueReplace"=>" ")/*DDMMAAAA nao obrigatorio informar zeros*/
+            ,"FEBRABAM5_G004"=>array("nCmp"=>"22.1","posInicio"=>"208","posFim"=>"240","leng"=>"33","Dec"=>"","type"=>"Alpha","Default"=>" ","valueReplace"=>" ")/* espaços em branco*/            
+        );
+$SeguimentoP = 
+        array(
+            "CodBancoComp_G001"=>array("nCmp"=>"1.1","posInicio"=>"1","posFim"=>"3","leng"=>"3","Dec"=>"","type"=>"Num","Default"=>"001","valueReplace"=>"0")/* 001 Banco do brasil*/
+            ,"LoteServico_G002"=>array("nCmp"=>"2.1","posInicio"=>"4","posFim"=>"7","leng"=>"4","Dec"=>"","type"=>"Num","Default"=>"0000","valueReplace"=>"0")
             
+            ,"TipoRegistro_G003"=>array("nCmp"=>"3.1","posInicio"=>"8","posFim"=>"8","leng"=>"1","Dec"=>"","type"=>"Num","Default"=>"1","valueReplace"=>"0")
             
         );
