4da173096eede0ff2ff2e6ba442335467f199a46
Andre <Andre@DESKTOP-21SQBJM>
2017-04-11 23:45:50 -0300

    Inicio - AnguBom

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+$arr = array ('CodBancoComp1_G001'=>'111','CodBancoComp2_G001'=>'111','LoteServico_G002' =>'2222','TipoRegistro_G003' =>'3','NumSeqRegLote_G038' => '44444');
+
+$test  = array_search(111,$arr);
+var_dump($test);
+
+function findKeyValue_Array($array,$chave,$valor){
+    foreach ($array as $key => $value) {
+        if(($key==$chave) and ($value==$valor)){
+            return true;            
+        }else{
+            return false;
+        }
+    }
+}
+
+var_dump(findKeyValue_Array($arr, "CodBancoComp1_G001", "111"));
